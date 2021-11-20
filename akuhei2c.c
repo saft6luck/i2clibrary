f35f908bfb54c6dbc886148090ce7bb45eb534d6
Szymon Bieganski <S.Bieganski@chello.nl>
2021-11-21 00:00:22 +0100

    douh! missing akuhei2c.{c,h}

---
+++ akuhei2c.c
@@ -0,0 +1,246 @@
+#include "akuhei2c.h"
+
+UBYTE
+clockport_read(pca9564_state_t *sp, UBYTE reg)
+{
+	UBYTE v;
+	UBYTE *ptr;
+
+	ptr = sp->cp + (reg * CLOCKPORT_STRIDE);
+	v = *ptr;
+#ifdef DEBUG
+	if (!(sp->in_isr))
+		printf("DEBUG: read %x from %p\n", (int) v, (void*) ptr);
+#endif /* DEBUG */
+
+	return v;
+}
+
+void
+clockport_write(pca9564_state_t *sp, UBYTE reg, UBYTE value)
+{
+	UBYTE *ptr;
+
+	ptr = (sp->cp) + (reg * CLOCKPORT_STRIDE);
+#ifdef DEBUG
+	if (!(sp->in_isr))
+		printf("DEBUG: write %x to %p\n", (int) value, (void*) ptr);
+#endif /* DEBUG */
+
+	*ptr = value;
+}
+
+void
+pca9564_write(pca9564_state_t *sp, UBYTE address, ULONG size, UBYTE **buf)
+{
+	sp->cur_op = EN_OP_WRITE;
+	pca9564_exec(sp, address, size, buf);
+	sp->cur_op = EN_OP_NOP;
+}
+
+void
+pca9564_read(pca9564_state_t *sp, UBYTE address, ULONG size, UBYTE **buf)
+{
+	sp->cur_op = EN_OP_READ;
+	pca9564_exec(sp, address, size, buf);
+	sp->cur_op = EN_OP_NOP;
+}
+
+void
+pca9564_exec(pca9564_state_t *sp, UBYTE address, ULONG size, UBYTE **buf)
+{
+	sp->slave_addr = address;
+	sp->buf_size = size;
+	sp->bytes_count = 0;
+	sp->buf = *buf;
+
+	pca9564_send_start(sp);
+
+	Wait(sp->sigmask_intr);
+
+	if (sp->cur_result != EN_RESULT_OK) {
+		//printf("OP: failed!\n");
+		pca9564_dump_state(sp);
+	}
+
+	sp->buf_size = 0;
+	sp->slave_addr = 0;
+}
+
+void
+pca9564_send_start(pca9564_state_t *sp)
+{
+	UBYTE c;
+
+	c = clockport_read(sp, I2CCON);
+	c |= I2CCON_STA|I2CCON_AA;
+	clockport_write(sp, I2CCON, c);	/* send START condition */
+
+}
+
+void
+pca9564_dump_state(pca9564_state_t *sp)
+{
+	UBYTE c, s, d;
+
+	c = clockport_read(sp, I2CCON);
+	s = clockport_read(sp, I2CSTA);
+	d = clockport_read(sp, I2CDAT);
+/*	printf("I2CCON: %x, I2CSTA: %x, I2CDAT: %x\n", c, s, d);
+	switch (s) {
+    case I2CSTA_SLAW_TX_ACK_RX:      0x18
+			printf("SLAW_TX_ACK_RX\n"); break;
+    case I2CSTA_DATA_TX_ACK_RX:      0x28
+			printf("DATA_TX_ACK_RX\n"); break;
+		case I2CSTA_DATA_TX_NACK_RX:    -0x30-
+			printf("DATA_TX_NACK_RX\n"); break;
+    case I2CSTA_SLAR_TX_ACK_RX:      0x40
+			printf("SLAR_TX_ACK_RX\n"); break;
+    case I2CSTA_SLAR_TX_NACK_RX:     0x48
+			printf("SLAR_TX_NACK_RX\n"); break;
+    case I2CSTA_DATA_RX_ACK_TX:      0x50
+			printf("DATA_RX_ACK_TX\n"); break;
+    case I2CSTA_DATA_RX_NACK_TX:     0x58
+			printf("DATA_RX_NACK_TX\n"); break;
+		case I2CSTA_SDA_STUCK:           0x70
+			printf("SDA_STUCK\n"); break;
+		case I2CSTA_SCL_STUCK:           0x90
+			printf("SCL_STUCK\n"); break;
+		default: break;
+	}*/
+}
+
+/* Interrupt service routine. */
+/*__saveds*/
+__interrupt
+void
+pca9564_isr(pca9564_state_t *sp __asm("a1"))
+{
+	UBYTE v;
+
+	sp->in_isr = TRUE;
+	sp->isr_called++;
+
+	if (!(clockport_read(sp, I2CCON) & I2CCON_SI)) {
+		sp->in_isr = FALSE;
+		return;
+	}
+
+	switch (sp->cur_op) {
+	case EN_OP_READ:
+		switch (clockport_read(sp, I2CSTA)) {
+		case I2CSTA_START_SENT:		/* 0x08 */
+			clockport_write(sp, I2CDAT, (sp->slave_addr << 1) | 1);
+			v = clockport_read(sp, I2CCON);
+			v &= ~(I2CCON_SI|I2CCON_STA);
+			clockport_write(sp, I2CCON, v);
+			break;
+		case I2CSTA_SLAR_TX_ACK_RX:	/* 0x40 */
+			v = clockport_read(sp, I2CCON);
+			v &= ~(I2CCON_SI);
+
+			if ((sp->bytes_count+1) < sp->buf_size)
+				v |= (I2CCON_AA);
+			else
+				v &= ~(I2CCON_AA); /* last byte */
+
+			clockport_write(sp, I2CCON, v);
+			break;
+		case I2CSTA_SLAR_TX_NACK_RX:	/* 0x48 */
+			sp->buf[sp->bytes_count] = clockport_read(sp, I2CDAT);
+			(sp->bytes_count)++;
+
+			v = clockport_read(sp, I2CCON);
+			v &= ~(I2CCON_SI);
+			v |= (I2CCON_AA|I2CCON_STO);	/* send stop */
+
+			clockport_write(sp, I2CCON, v);
+
+			sp->cur_result = EN_RESULT_ERR;
+			Signal(sp->MainTask, sp->sigmask_intr);
+			break;
+		case I2CSTA_DATA_RX_ACK_TX:	/* 0x50 */
+			sp->buf[sp->bytes_count] = clockport_read(sp, I2CDAT);
+			(sp->bytes_count)++;
+
+			v = clockport_read(sp, I2CCON);
+			v &= ~(I2CCON_SI);
+
+			if ((sp->bytes_count+1) < sp->buf_size)
+				v |= (I2CCON_AA);
+			else
+				v &= ~(I2CCON_AA); /* last byte */
+
+			clockport_write(sp, I2CCON, v);
+			break;
+		case I2CSTA_DATA_RX_NACK_TX:	/* 0x58 */
+			sp->buf[sp->bytes_count] = clockport_read(sp, I2CDAT);
+			(sp->bytes_count)++;
+
+			v = clockport_read(sp, I2CCON);
+			v &= ~(I2CCON_SI);
+			v |= (I2CCON_AA|I2CCON_STO);	/* send stop */
+
+			clockport_write(sp, I2CCON, v);
+
+			sp->cur_result = EN_RESULT_OK;
+			Signal(sp->MainTask, sp->sigmask_intr);
+			break;
+		default:
+			clockport_write(sp, I2CCON, 0);
+			sp->cur_result = EN_RESULT_ERR;
+			Signal(sp->MainTask, sp->sigmask_intr);
+			break;
+		}
+		break;
+	case EN_OP_WRITE:
+		switch (clockport_read(sp, I2CSTA)) {
+		case I2CSTA_START_SENT:		/* 0x08 */
+			clockport_write(sp, I2CDAT, (sp->slave_addr << 1) | 0);
+			v = clockport_read(sp, I2CCON);
+			v &= ~(I2CCON_SI|I2CCON_STA);
+			clockport_write(sp, I2CCON, v);
+			break;
+		case I2CSTA_SLAW_TX_ACK_RX:	/* 0x18 */
+			clockport_write(sp, I2CDAT, sp->buf[sp->bytes_count]);
+			v = clockport_read(sp, I2CCON);
+			v &= ~(I2CCON_SI);
+			clockport_write(sp, I2CCON, v);
+			break;
+		case I2CSTA_DATA_TX_ACK_RX:	/* 0x28 */
+		/*case I2CSTA_DATA_TX_NACK_RX:*/    /* 0x30 */
+			v = clockport_read(sp, I2CCON);
+
+			(sp->bytes_count)++;
+			if (sp->bytes_count < sp->buf_size) {
+				clockport_write(sp, I2CDAT, sp->buf[sp->bytes_count]);
+
+			} else {
+				v |= (I2CCON_STO);
+			}
+
+			v &= ~(I2CCON_SI);
+			clockport_write(sp, I2CCON, v);
+
+			if (sp->bytes_count == sp->buf_size) {
+				sp->cur_result = EN_RESULT_OK;
+				Signal(sp->MainTask, sp->sigmask_intr);
+			}
+
+			break;
+		default:
+			clockport_write(sp, I2CCON, 0);
+			sp->cur_result = EN_RESULT_ERR;
+			Signal(sp->MainTask, sp->sigmask_intr);
+			break;
+		}
+		break;
+	case EN_OP_NOP:
+		clockport_write(sp, I2CCON, 0);
+		sp->cur_result = EN_RESULT_ERR;
+		Signal(sp->MainTask, sp->sigmask_intr);
+		break;
+	}
+
+	sp->in_isr = FALSE;
+}
---
+++ akuhei2c.h
@@ -0,0 +1,111 @@
+#ifndef AKUHEI2C_H
+#define AKUHEI2C_H
+
+#include <stdio.h>
+/*#include <stdlib.h>*/
+
+#include <proto/exec.h>
+#include <proto/dos.h>
+
+#include <exec/types.h>
+#include <exec/memory.h>
+#include <exec/interrupts.h>
+
+#include <hardware/intbits.h>
+
+//#include "SDI_interrupt.h"
+//#include "SDI_compiler.h"
+
+#define DBG                     1
+
+#define CLOCKPORT_BASE          (UBYTE *)0xD80001
+#define CLOCKPORT_STRIDE        4
+
+#define I2CSTA                  0
+#define I2CTO                   0
+#define I2CDAT                  1
+#define I2CADR                  2
+#define I2CCON                  3
+
+#define I2CCON_CR0              (1 << 0)
+#define I2CCON_CR1              (1 << 1)
+#define I2CCON_CR2              (1 << 2)
+#define I2CCON_CR_330KHZ        (0x0)
+#define I2CCON_CR_288KHZ        (0x1)
+#define I2CCON_CR_217KHZ        (0x2)
+#define I2CCON_CR_146KHZ        (0x3)
+#define I2CCON_CR_88KHZ         (0x4)
+#define I2CCON_CR_59KHZ         (0x5)
+#define I2CCON_CR_MASK          (0x7)
+#define I2CCON_SI               (1 << 3)
+#define I2CCON_STO              (1 << 4)
+#define I2CCON_STA              (1 << 5)
+#define I2CCON_ENSIO            (1 << 6)
+#define I2CCON_AA               (1 << 7)
+
+#define I2CSTA_START_SENT       0x08
+
+#define I2CSTA_SLAR_TX_ACK_RX   0x40
+#define I2CSTA_SLAR_TX_NACK_RX  0x48
+#define I2CSTA_DATA_RX_ACK_TX   0x50
+#define I2CSTA_DATA_RX_NACK_TX  0x58
+
+#define I2CSTA_SLAW_TX_ACK_RX   0x18
+#define I2CSTA_SLAW_TX_NACK_RX  0x20
+#define I2CSTA_DATA_TX_ACK_RX   0x28
+#define I2CSTA_DATA_TX_NACK_RX  0x30
+
+#define I2CSTA_IDLE             0xF8
+#define I2CSTA_SDA_STUCK        0x70
+#define I2CSTA_SCL_STUCK        0x90
+
+#define EN_OP_NOP               0xF0
+#define EN_OP_READ              0xF1
+#define EN_OP_WRITE             0xF2
+
+/*typedef enum {
+        OP_NOP,
+        OP_READ,
+        OP_WRITE
+} op_t;*/
+
+#define EN_RESULT_OK            0xE0
+#define EN_RESULT_ERR           0xE1
+
+/*typedef enum {
+        RESULT_OK,
+        RESULT_ERR
+} result_t;*/
+
+/* glorious god object that holds the state of everything in this program; tldr */
+typedef struct {
+        UBYTE cur_op;
+        UBYTE cur_result;
+
+        UBYTE *cp;
+
+        BYTE sig_intr;
+        LONG sigmask_intr;
+        struct Task *MainTask;
+
+        UBYTE *buf;
+        ULONG buf_size;
+        ULONG bytes_count;
+
+        UBYTE slave_addr;
+/*#ifdef DEBUG*/
+        int isr_called; /* how may times ISR was called */
+        BOOL in_isr;
+/*#endif  DEBUG */
+} pca9564_state_t;
+
+UBYTE clockport_read(pca9564_state_t *, UBYTE);
+void clockport_write(pca9564_state_t *, UBYTE, UBYTE);
+__interrupt void pca9564_isr(pca9564_state_t * __asm("a1"));
+void pca9564_dump_state(pca9564_state_t *);
+void pca9564_send_start(pca9564_state_t *);
+void pca9564_read(pca9564_state_t *, UBYTE, ULONG, UBYTE **);
+void pca9564_write(pca9564_state_t *, UBYTE, ULONG, UBYTE **);
+void pca9564_exec(pca9564_state_t *, UBYTE, ULONG, UBYTE **);
+
+#endif      /* AKUHEI2C_H */
